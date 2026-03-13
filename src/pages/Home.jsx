b2f7465d74a8db84ea5b1e7f1c8fb403bca4f31c
Kevin Sun <kevinsalvadordeanianos@gmail.com>
2026-03-14 00:52:30 +0200

    import do StatsBar revolvido na HOME

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import Nav from "../components/layouts/Nav";
 import Hero from "../components/sections/Hero";
-import StatsBar from "../components/layouts/Statsbar";
+import StatsBar from "../components/layouts/StatsBar";
 import About from "../components/sections/About";
 import MenuSection from "../components/sections/MenuSection";
 import ChefSection from "../components/sections/ChefSection";
